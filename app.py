0d99b657cb2250f78175b402bceea7882263d18d
Sadiq <sadiqy93@gmail.com>
2025-11-30 10:58:59 -0500

    cleaning project

--- app.py
+++
@@ -1,9 +0,0 @@
-# app.py (Main Entry Point)
-from flask import Flask
-from api.routes import bp as api_bp
-
-app = Flask(__name__)
-app.register_blueprint(api_bp)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
