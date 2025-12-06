1c242c50fb5756fba54796821b0e03722dfbb8f5
Sadiq <sadiqy93@gmail.com>
2025-12-06 08:21:03 -0500

    cleaning up project

--- betting/picks.py
+++
@@ -1,7 +0,0 @@
-# betting/picks.py
-import pandas as pd
-
-def generate_picks(preds_df, threshold=0.6):
-    picks = preds_df[preds_df["pred_home_win_prob"] >= threshold].copy()
-    picks["pick"] = "home"
-    return picks
